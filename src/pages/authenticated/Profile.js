e684423e4e99964e1681f73e9f069738f9fbc71f
I-am-rish <ak3754732@gmail.com>
2023-12-28 18:58:46 +0530

    update

--- src/pages/authenticated/Profile.js
+++ src/pages/authenticated/Profile.js
@@ -1,12 +1,43 @@
-import React from "react";
-import Loader from "../../components/loader/Loader";
+import React, { useEffect, useState } from "react";
+// import Loader from "../../components/loader/Loader";
+import httpClient from "../../util/HttpClient";
+import axios from "axios";
 
 const Profile = () => {
+  const [file, setFile] = useState();
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // if (!file || !(file instanceof File)) return;
+    console.log(file);
+    let formData = new FormData();
+    formData.append("avatar", file);
+
+    httpClient
+      .post("/user/profile/avatar", formData, {
+        // headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res) => {
+        // console.log("Profile image uploaded successfully!");
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div>
-      Profile
-     <Loader />
-    </div>
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="file"
+          name="avatar"
+          filename={file}
+          onChange={(e) => setFile(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+    </>
   );
 };
 
